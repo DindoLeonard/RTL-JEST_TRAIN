3d1599d4557bb5de9ec8a3549fec0c0f65c1cb1b
DindoLeonard <dindo.leonard@gmail.com>
2021-12-28 22:09:23 +0800

    husky and lint-staged

--- src/__test__/Assertion.test.tsx
+++ src/__test__/Assertion.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Assertion from '../example-code/Assertions';
 
 test('renders Hello World', () => {
